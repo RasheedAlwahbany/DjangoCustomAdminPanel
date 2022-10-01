207b4bd1f855b00b0b0d061baba623fde15a72d9
RasheedAlwahbanyMinsati <rasheed.alwahbany@minsati.com>
2022-10-01 14:08:33 +0300

    Complete adding action with search & ordering options

--- user/admin.py
+++ user/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from user.models import User
 
 # Register your models here.
+admin.site.site_header = "Custome Admin Action"
+admin.site.site_title = "Custome Admin Action"
+admin.site.index_title = "Welcome to Custome Admin Action"
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -11,4 +14,18 @@ class UserAdmin(admin.ModelAdmin):
     Args:
         admin (_type_): _description_
     """
+    model = User
+    fields = ["full_name","email","phone"]
+    list_display = ("full_name","email","phone")
+    search_fields = ['full_name','email']
+    actions = ['set_phone']
+    ordering = ['full_name','email']
+    
+    def set_phone(self, request, queryset):
+        queryset.update(phone = 000000000)
+
+        
+            
+    
+    
     
--- user/models.py
+++ user/models.py
@@ -6,6 +6,6 @@ from django.db import models
 class User(models.Model):
     full_name = models.CharField(verbose_name="Full Name",max_length=100)
     email = models.EmailField(verbose_name="Email")
-    Phone = models.IntegerField(verbose_name="Phone")
+    phone = models.IntegerField(verbose_name="Phone")
     
     
